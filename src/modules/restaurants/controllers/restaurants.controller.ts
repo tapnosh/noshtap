7171eff1ba0e4424a7890e839d3dc2f736c9794c
Filip M. <34985691+popa3@users.noreply.github.com>
2025-06-08 18:00:03 +0200

    feat: implement update endpoint for restaurant
    re #14

--- src/modules/restaurants/controllers/restaurants.controller.ts
+++ src/modules/restaurants/controllers/restaurants.controller.ts
@@ -1,21 +1,34 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Param, Req, UseGuards } from '@nestjs/common';
 import { RestaurantsService } from '../services/restaurants.service';
 import { CreateRestaurantDto } from '../dto/create-restaurant.dto';
+import { UpdateRestaurantDto } from '../dto/update-restaurant.dto';
 import { Restaurant } from '@prisma/client';
 import { Public } from 'src/decorators/public.decorator';
+import { ClerkAuthGuard } from 'src/modules/auth/clerk-auth.guard';
 
 @Controller('restaurants')
 export class RestaurantsController {
   constructor(private readonly restaurantsService: RestaurantsService) { }
 
   @Post()
-  create(@Body() createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
-    return this.restaurantsService.create(createRestaurantDto);
+  @UseGuards(ClerkAuthGuard)
+  create(@Body() createRestaurantDto: CreateRestaurantDto, @Req() req: any,): Promise<Restaurant> {
+    return this.restaurantsService.create(createRestaurantDto, req.auth.userId);
   }
 
   @Get()
   @Public()
   findAll(): Promise<Restaurant[]> {
     return this.restaurantsService.findAll();
+  }
+
+  @Put(':id')
+  @UseGuards(ClerkAuthGuard)
+  update(
+    @Param('id') id: string,
+    @Body() dto: UpdateRestaurantDto,
+    @Req() req: any,
+  ){
+    return this.restaurantsService.update(id, dto, req.auth.userId);
   }
 }
--- src/modules/restaurants/services/restaurants.service.ts
+++ src/modules/restaurants/services/restaurants.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateRestaurantDto } from "../dto/create-restaurant.dto";
+import { UpdateRestaurantDto } from "../dto/update-restaurant.dto";
 import { Restaurant, Prisma } from "@prisma/client";
+import { ForbiddenException, NotFoundException } from "@nestjs/common";
 
 @Injectable()
 export class RestaurantsService {
     constructor(private prisma: PrismaService) { }
 
-    async create(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+    async create(createRestaurantDto: CreateRestaurantDto, userId: string): Promise<Restaurant> {
         const data: Prisma.RestaurantCreateInput = {
             name: createRestaurantDto.name,
             description: createRestaurantDto.description,
+            ownerId: userId,
             theme: { connect: { id: createRestaurantDto.theme_id } },
             address: createRestaurantDto.address ? {
                 create: {
@@ -42,6 +45,61 @@ export class RestaurantsService {
                     }
                 }
             }
+        });
+    }
+
+    async update(
+        id: string, 
+        dto: UpdateRestaurantDto,
+        userId: string
+    ): Promise<Restaurant> {
+        const restaurant = await this.prisma.restaurant.findUnique({
+            where: { id },
+            include: { images: true, categories: true },
+        });
+
+        if (!restaurant) {
+            throw new NotFoundException('Restauracja nie istnieje.');
+        }
+        
+        if (restaurant.ownerId !== userId) {
+            throw new ForbiddenException('Nie masz dostępu do tej restauracji.');
+        }
+        
+        await this.prisma.restaurantImages.deleteMany( { where: { restaurant_id: id }});
+        await this.prisma.restaurantCategoryRelation.deleteMany( { where: { restaurant_id: id }})
+
+        await this.prisma.restaurantImages.createMany({
+            data: dto.images.map((url) => ({
+                restaurant_id: id,
+                image_url: url,
+            })),
+        });
+
+        await this.prisma.restaurantCategoryRelation.createMany({
+            data: dto.category_ids.map((categoryId) => ({
+                restaurant_id: id,
+                category_id: categoryId,
+            })),
+        });
+
+        return this.prisma.restaurant.update({
+            where: { id },
+            data: {
+                name: dto.name,
+                description: dto.description,
+                theme: { connect: { id: dto.theme_id }},
+            },
+            include: {
+                theme: true,
+                address: true,
+                images: true,
+                categories: {
+                    include: {
+                        category: true,
+                    },
+                },
+            },
         });
     }
 
