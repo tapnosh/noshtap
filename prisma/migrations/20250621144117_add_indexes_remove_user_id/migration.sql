82a085b844340ec7676a2ed4271ba9c5689a9ea4
Michał Nowak <mhnowakk@gmail.com>
2025-06-21 20:54:29 +0200

    fix: migration for menu

--- prisma/migrations/20250621144117_add_indexes_remove_user_id/migration.sql
+++ prisma/migrations/20250621144117_add_indexes_remove_user_id/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the column `owner_id` on the `RestaurantMenu` table. All the data in the column will be lost.
 
 */
--- AlterTable
-ALTER TABLE "RestaurantMenu" DROP COLUMN "owner_id";
-
 -- CreateIndex
 CREATE INDEX "RestaurantMenu_restaurant_id_is_deleted_idx" ON "RestaurantMenu"("restaurant_id", "is_deleted");
 
