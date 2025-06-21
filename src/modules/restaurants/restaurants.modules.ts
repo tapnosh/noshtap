61ef709a3b8c30f4e63fd8260e2135d319d7a8a7
Michał Nowak <mhnowakk@gmail.com>
2025-06-21 21:27:05 +0200

    fix: modules

--- src/modules/restaurants/restaurants.modules.ts
+++ src/modules/restaurants/restaurants.modules.ts
@@ -8,11 +8,14 @@ import { ThemesController } from "./controllers/themes.controller";
 import { ThemesService } from "./services/themes.service";
 import { MenusService } from "./services/menus.service";
 import { MenusController } from "./controllers/menus.controller";
+import { QrController } from "./controllers/qr.controller";
+import { QrService } from "./services/qr.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [RestaurantsController, CategoriesController, ThemesController, MenusController],
-  providers: [RestaurantsService, CategoriesService, ThemesService, MenusService],
-  exports: [RestaurantsService, CategoriesService, ThemesService, MenusService],
+  imports: [PrismaModule, ConfigModule],
+  controllers: [RestaurantsController, CategoriesController, ThemesController, MenusController, QrController],
+  providers: [RestaurantsService, CategoriesService, ThemesService, MenusService, QrService],
+  exports: [RestaurantsService, CategoriesService, ThemesService, MenusService, QrService],
 })
 export class RestaurantsModule { }
